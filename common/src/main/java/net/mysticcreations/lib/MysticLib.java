cb8c6dd1635dc6fae19a2cf9f9f56c242d0f4419
JustMili <rodx160@gmail.com>
2026-02-19 20:41:16 +0100

    Added log manager, how did we forgor?

--- common/src/main/java/net/mysticcreations/lib/MysticLib.java
+++ common/src/main/java/net/mysticcreations/lib/MysticLib.java
@@ -1,8 +1,11 @@
 package net.mysticcreations.lib;
 
 import net.minecraft.resources.ResourceLocation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public final class MysticLib {
+    public static final Logger LOGGER = LogManager.getLogger(MysticLib.class);
     public static final String MODID = "mysticlib";
 
     public static void init() {
